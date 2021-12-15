9493576fac8f0b288298effc04b51ec4d7eca985
Andres Arevalo <andresfelipear@gmail.com>
2021-12-14 18:26:30 -0800

    learning how to custom theme.palette

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,32 +8,50 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import RefreshIcon from '@mui/icons-material/Refresh';
 
+
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = createTheme({
+
+    palette: {
+        primary: {
+            main: "#1976d2",
+            custom: "#3f51b5",
+        },
+    },
+});
+
 function Header() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Angular 9 MatTable CRUD Example
-                    </Typography>
-                    <Box sx={{ display: { xs: 'none', md: 'flex' } }} className="HeaderRight">
+        <ThemeProvider theme={theme}>
+            <Box sx={{ flexGrow: 1 }} >
+                <AppBar position="static" className="main-header" sx={{ bgcolor: "primary.custom" }} >
+                    <Toolbar>
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            Reload data:
+                            Angular 9 MatTable CRUD Example
                         </Typography>
-                        <IconButton
-                            size="small"
-                            edge="start"
-                            color="inherit"
-                            aria-label="refresh"
-                            sx={{ ml: 0.5 }}
-                        >
-                            <RefreshIcon />
-                        </IconButton>
-                    </Box>
+                        <Box sx={{ display: { xs: 'none', md: 'flex' } }} className="HeaderRight">
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                                Reload data:
+                            </Typography>
+                            <IconButton
+                                size="small"
+                                edge="start"
+                                color="inherit"
+                                aria-label="refresh"
+                                sx={{ ml: 0.5 }}
+                            >
+                                <RefreshIcon />
+                            </IconButton>
+                        </Box>
 
-                </Toolbar>
-            </AppBar>
-        </Box>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+
+        </ThemeProvider>
+
     )
 }
 
