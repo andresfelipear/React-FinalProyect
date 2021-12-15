c654220f379705fc0b63a27902ecdce1d5ba5da0
Andres Arevalo <andresfelipear@gmail.com>
2021-12-14 17:41:07 -0800

    css, front 'header' done! No functionality yet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import DeleteIcon from '@mui/icons-material/Delete';
+import "./Header.css"
 
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import RefreshIcon from '@mui/icons-material/Refresh';
 
 function Header() {
@@ -18,17 +16,16 @@ function Header() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Angular 9 MatTable CRUD Example
                     </Typography>
-
-                    <Box>
+                    <Box sx={{ display: { xs: 'none', md: 'flex' } }} className="HeaderRight">
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                             Reload data:
                         </Typography>
                         <IconButton
-                            size="large"
+                            size="small"
                             edge="start"
                             color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
+                            aria-label="refresh"
+                            sx={{ ml: 0.5 }}
                         >
                             <RefreshIcon />
                         </IconButton>
