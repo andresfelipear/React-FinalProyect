284c1ba560457d198e13536bebcd137c03ab8b6c
Andres Arevalo <andresfelipear@gmail.com>
2021-12-15 17:23:17 -0800

    main component "elevation(8)"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Paper} from "@mui/material"
+
+function Main() {
+    return (
+        <Paper elevation={8}>
+            Hola
+        </Paper>
+    )
+}
+
+export default Main
